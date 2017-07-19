ac4c04da0c5d76f95bc152a537de6a8039d280e0
hyqLeonardo <hyq335335@163.com>
2017-07-19 22:59:24 +0800

    cleaned

--- yearonequant/backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# copy important files to share directory
-cp ./*.py ./*.ipynb ./*.csv ../../share/
-
-# print current date and time, will be redirected into log file when called by cron
-now="$(date)"
-printf "******** Backup done at %s ********\n" "$now"
