2b520d660d312983f528bf0d04ba6a91f66f2b0b
hyqLeonardo <hyq335335@163.com>
2017-07-19 23:37:07 +0800

    works correctly in jupyter, not in shell

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='yearonequant',
-      version='1.0',
-      description='Think as quant, tade as quant',
+      version='1.2',
+      description='Think as quant, trade as quant',
       url='http://github.com/hyqLeonardo/yearonequant',
       author='Leonardo',
       author_email='yearonequant@163.com',
